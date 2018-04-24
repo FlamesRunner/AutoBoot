bccfd158b68716f52adfd6a39056ff457c419d25
Andrew H <FlamesRunner@users.noreply.github.com>
2018-04-24 17:57:26 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+// Configuration
+////////////////////////////////////////////////////////////////////////////////
+// To add more servers, simply copy the following three lines and change the ID.
+// Then, make sure to input the hash, key and host.
+// The host should include the command file location.
+// Set ignore to true if you wish to skip a server.
+// Example: $cfgservers[0]["ignore"] = true;
+////////////////////////////////////////////////////////////////////////////////
+
+$cfgservers[0]["host"] = "https://solusvm.example.com/api/client/command.php";
+$cfgservers[0]["hash"] = "hash"; 
+$cfgservers[0]["key"] = "key";
+?>
